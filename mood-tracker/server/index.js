9a320939aa3641c2e043fc134d48dc3b4392094c
[soumashree das] <[soumshreedas2004@gmail.com]>
2025-05-04 21:31:31 +0530

    link

--- mood-tracker/server/index.js
+++ mood-tracker/server/index.js
@@ -8,9 +8,9 @@ import JournalRouters from "./src/routes/journalEntry.routes.js";
 const app = express();
 
 app.use(cors({
-    origin: 'https://full-stack-projects-nine.vercel.app/', // Your frontend URL
-    credentials: true // If using cookies/sessions
-  }));
+    origin:'https://full-stack-projects-nine.vercel.app/',
+    credentials:true
+}))
 
 app.options("*", cors()); // enable pre-flight for all routes
 
